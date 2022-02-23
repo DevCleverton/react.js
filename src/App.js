acf61f4c8148cc161cae4c46f16594269847075d
Cleverton da Rocha <56972170+DevCleverton@users.noreply.github.com>
2022-02-23 18:53:58 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
 import React, { useState } from "react";
-import "./styles.css";
-import GoogleLogin from "react-google-login";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [url, setUrl] = useState("");
+  const [loginStatus, setLoginStatus] = useState(false);
 
-const [name, setName] = useState("");
-const [email, setEmail] = useState("");
-const [url, setUrl] = useState("");
-const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-const responseGoogle = response => {
-console.log(response);
-setName(response.profileObj.name);
-setEmail(response.profileObj.email);
-setUrl(response.profileObj.imageUrl);
-};
-
-return (
-<div className="App">
-<h1>Login with Google</h1>
-<h2>Welcome: {name}</h2>
-<h2>Email: {email}</h2>
-<img src={url} alt={name} />
-<GoogleLogin
-clientId="208739294372-0s28mtjnq92um8g6argnn894oa01ls8o.apps.googleusercontent.com"
-buttonText="Login"
-onSuccess={responseGoogle}
-onFailure={responseGoogle}
-cookiePolicy={"single_host_origin"}
-/>
-</div>
-);
-
+  const responseGoogle = response => {
+    console.log(response);
+    setName(response.profileObj.name);
+    setEmail(response.profileObj.email);
+    setUrl(response.profileObj.imageUrl);
+    setLoginStatus(true);
+  };
+  const logout = () => {
+    console.log("logout");
+    setLoginStatus(false);
+  };
+  return (
+    <div className="App">
+      <h1>Login with Google</h1>
+      {!loginStatus && (
+        <GoogleLogin
+          clientId="208739294372-0s28mtjnq92um8g6argnn894oa01ls8o.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      )}
+      {loginStatus && (
+        <div>
+          <h2>Welcome {name}</h2>
+          <h2>Email: {email}</h2>
+          <img src={url} alt={name} />
+          <br />
+          <GoogleLogout
+            clientId="208739294372-0s28mtjnq92um8g6argnn894oa01ls8o.apps.googleusercontent.com"
+            buttonText="Logout"
+            onLogoutSuccess={logout}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
+
